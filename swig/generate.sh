7df56558bb84c5645f37a7eace58920cec51d50a
Oliver Rettig <Oliver.Rettig@orat.de>
2020-10-21 09:08:42 +0200

    Folder moved corresponding to convention

--- swig/generate.sh
+++ swig/generate.sh
@@ -4,11 +4,11 @@ cd "$(dirname "$BASH_SOURCE")"
 
 
 
-mkdir -p "../JAVA/src/main/java/de/dhbw/rahmlab/rtde/impl/"
+mkdir -p "../JRTDE/src/main/java/de/dhbw/rahmlab/rtde/impl/"
 
 
 #-debug-tmsearch
-swig -Wall -c++ -java -package de.dhbw.rahmlab.rtde.impl -outdir "../JAVA/src/main/java/de/dhbw/rahmlab/rtde/impl/" -o RTDE_wrap.cpp PlatformIndependent.i
+swig -Wall -c++ -java -package de.dhbw.rahmlab.rtde.impl -outdir "../JRTDE/src/main/java/de/dhbw/rahmlab/rtde/impl/" -o RTDE_wrap.cpp PlatformIndependent.i
 
 echo "swig finished"
 
