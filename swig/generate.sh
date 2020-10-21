239fc7375404aace187dbcae81457972e23d2166
Oliver Rettig <Oliver.Rettig@orat.de>
2020-10-21 08:53:32 +0200

    RTDE-->rtde corresponsing to java convention

--- swig/generate.sh
+++ swig/generate.sh
@@ -4,11 +4,11 @@ cd "$(dirname "$BASH_SOURCE")"
 
 
 
-mkdir -p "../JAVA/src/main/java/de/dhbw/rahmlab/RTDE/impl/"
+mkdir -p "../JAVA/src/main/java/de/dhbw/rahmlab/rtde/impl/"
 
 
 #-debug-tmsearch
-swig -Wall -c++ -java -package de.dhbw.rahmlab.RTDE.impl -outdir "../JAVA/src/main/java/de/dhbw/rahmlab/RTDE/impl/" -o RTDE_wrap.cpp PlatformIndependent.i
+swig -Wall -c++ -java -package de.dhbw.rahmlab.rtde.impl -outdir "../JAVA/src/main/java/de/dhbw/rahmlab/rtde/impl/" -o RTDE_wrap.cpp PlatformIndependent.i
 
 echo "swig finished"
 
