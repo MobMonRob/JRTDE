6e52f1948bf82b7a2bb1c82cfe20e23159822e9c
Oliver Rettig <Oliver.Rettig@orat.de>
2020-12-04 16:06:48 +0100

    test further bindings and external build libs added

--- Linux64/generate_test.sh
+++ Linux64/generate_test.sh
@@ -4,11 +4,11 @@ cd "$(dirname "$BASH_SOURCE")"
 
 ./clear.sh
 
-../swig/generate.sh
+../swig/generate_test.sh
 
 #https://web.archive.org/web/20201007174747/https://stackoverflow.com/questions/54373254/load-dependent-so-from-other-shared-library-via-jni/58842312#58842312
 # -rpath,'$ORIGIN/.' --> ad the folder where the so-file resides into the search path for dependend libs
-gcc -shared -fPIC -cpp -std=c++17 -O3 -flto ../swig/rtde_wrap.cpp -Lur_rtde/libs -o libjrtde.so -I/usr/lib/jvm/default-java/include -I/usr/lib/jvm/default-java/include/linux -I. -Wl,-rpath,'$ORIGIN/.'
+/usr/bin/g++-7 -shared -fPIC -cpp -std=c++1z -O3 -flto ../swig/rtde_wrap.cpp -Llibs -lurcl -o libjrtde.so -I/usr/lib/jvm/default-java/include -I/usr/lib/jvm/default-java/include/linux -I. -Wl,-rpath,'$ORIGIN/.'
 
 echo "gcc finished"
 
@@ -16,6 +16,9 @@ echo "gcc finished"
 mkdir -p ../JRTDE/natives/linux-amd64/
 cp ./*.so* ../JRTDE/natives/linux-amd64/
 cp ./build/*.so* ../JRTDE/natives/linux-amd64/
+
+# WORKAROUND
+sudo cp libjrtde.so /usr/java/packages/lib
 
 echo "Linux64 finished"
 
